db7895303d29ca37497e39a632009a29a244f0c4
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-01-24 18:52:57 -0500

    Update lab02.cpp

--- Lab02/lab02.cpp
+++ Lab02/lab02.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
 
+int main(){
+    int num, b = 0, persistence = 0, c=0;
+    cin >> num;
+    if (num > 0 && num < 10){
+        cout << " 0 " << num << endl;
+        return 0;
+    }
+    if (num <=0) {
+        cout << "Error" << endl;
+        return 0;
+    }
+    if (num >=10) {
+        while (num >= 10){
+            b = num % 10;
+            c += b;
+            num /= 10;
+            if (num < 10 ){
+                num +=c ;
+                persistence+= 1;
+                c=0;
+            }
+        }
+    }
+    
+    cout << persistence << " " << num << endl;
+    return 0;
+}
