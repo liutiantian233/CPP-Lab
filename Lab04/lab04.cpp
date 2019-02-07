4558357102041fb48fbe824c925523245e89be3c
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-02-07 13:18:34 -0500

    Update lab04.cpp

--- Lab04/lab04.cpp
+++ Lab04/lab04.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using std::cout; using std::endl;
 
+#include <string>
+using std::string;
+
+// note you need algorithm and sort
+#include <algorithm>
+using std::sort;
+
+/*
+sort individual characters of a string
+*/
