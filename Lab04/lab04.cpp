e2427f6c5a4283f048aac6b6f61546cab8f27c39
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-02-08 00:41:08 -0500

    Update lab04.cpp

--- Lab04/lab04.cpp
+++ Lab04/lab04.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using std::cout; using std::endl;
+using std::cin; using std::cout; using std::endl;
 
 #include <string>
 using std::string;
@@ -8,13 +8,48 @@ using std::string;
 #include <algorithm>
 using std::sort;
 
-/*
-sort individual characters of a string
-*/
+#include <cmath>
+
+long loc_to_dec(string loc) {
+    auto dec = 0;
+    auto exp = 0;
+    for (char c : loc) {
+        exp = c - 'a';
+        dec += pow(2, exp);
+    }
+    return dec;
+}
+
+string abbreviate(string loc) {
+    for (unsigned i = 0; i < loc.size(); ++i) {
+        while (loc[i] == loc[i + 1]) {
+            loc.replace(i, 2, 1, char(loc[i] + 1));
+            sort(loc.begin(), loc.end());
+        }
+    }
+    return loc;
+}
+
+string dec_to_loc(long dec) {
+    string str;
+    for (int i = 0; i < dec; ++i)
+        str.push_back('a');
+    return abbreviate(str);
+}
+
+long add_loc(string loc1, string loc2) {
+    string loc = loc1 + loc2;
+    sort(loc.begin(), loc.end());
+    loc = abbreviate(loc);
+    return loc_to_dec(loc);
+}
 
 int main() {
-  string my_str = "aebcd";
-  cout << "String was:" << my_str;
-  sort(my_str.begin(), my_str.end());
-  cout << ", and sorted is:" << my_str <<endl;
+    string loc;
+    long num;
+    cin >> loc >> num;
+    cout << loc_to_dec(loc) << " ";
+    cout << abbreviate(loc) << " ";
+    cout << dec_to_loc(num) << " ";
+    cout << add_loc(abbreviate(loc), abbreviate(loc)) << endl;
 }
