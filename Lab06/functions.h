284b83107e02dded950a8153836686e38ed1b141
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-02-21 16:05:55 -0500

    Update functions.h

--- Lab06/functions.h
+++ Lab06/functions.h
@@ -1,1 +1,19 @@
+#ifndef LAB06_FUNCTIONS_H
+#define LAB06_FUNCTIONS_H
 
+#include <iostream>
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> split(const string &s, char separator = ' ');
+
+vector<long> vector_ops(const vector<long> &v1, const vector<long> &v2, char op);
+
+void print_vector(ostream &out, const vector<string> &v);
+
+void print_vector(ostream &out, const vector<long> &v);
+
+#endif //LAB06_FUNCTIONS_H
