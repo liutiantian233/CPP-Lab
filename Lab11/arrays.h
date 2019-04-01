b662a4ad62e15330c0ec11b5ac60851a1dbf18b5
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-03-31 21:13:56 -0400

    Create arrays.h

---
+++ Lab11/arrays.h
@@ -0,0 +1,23 @@
+#ifndef LAB11_ARRAYS_H
+#define LAB11_ARRAYS_H
+
+#include <string>
+using std::string;
+
+#include <utility>
+using std::pair;
+
+#include <iostream>
+using std::ostream;
+
+bool is_even(long l);
+
+void print_array(long ary[], size_t sz, ostream &out);
+
+size_t fill_from_file(long *(&ary), string f_name);
+
+size_t concatenate(long *(&ary1), size_t sz1, long ary2[], size_t sz2);
+
+pair<long*, size_t> copy_evens(long ary[], size_t sz);
+
+#endif //LAB11_ARRAYS_H
