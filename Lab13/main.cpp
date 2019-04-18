f35d3ed30e7d242cf9b4ecc150db91babfa2a5f3
TianTian <44875235+liutiantian233@users.noreply.github.com>
2019-04-18 16:27:54 -0400

    Create main.cpp

---
+++ Lab13/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout; using std::endl; using std::ostream;
+
+#include <string>
+using std::string;
+
+#include "singlelink.h"
+
+int main() {
+
+    SingleLink<string> singleLink;
+    // write test code below
+}
